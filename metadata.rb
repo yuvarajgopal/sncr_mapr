098a35c4d3e95d405c8257c0fc6adbc7622db0f4
Yuvaraj Gopal <yuvaraj.gopal@synchronoss.com>
2017-01-06 20:34:41 -0500

    updated dependency

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,9 @@ maintainer_email ''
 license          'All rights reserved'
 description      'Installs/Configures mapr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.13.3'
+version          '0.1.0'
 
-%w( build-essential iptables java limits line ntp
-    sysctl selinux sudo users yum ).each do |cb|
+%w( java sudo users yum ).each do |cb|
   depends cb
 end
 
