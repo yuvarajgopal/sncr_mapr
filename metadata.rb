9f1429285a6e20324f0f7d36b36e835e5ecbcc45
Yuvaraj Gopal <yuvaraj.gopal@synchronoss.com>
2017-01-10 09:09:44 -0500

    updated version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ''
 license          'All rights reserved'
 description      'Installs/Configures mapr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 %w( java sudo users yum ).each do |cb|
   depends cb
