2badda42bea96a4354c24da81ea7c29b224ed8d3
Yuvaraj Gopal <yuvaraj.gopal@synchronoss.com>
2017-01-10 09:04:10 -0500

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'sncr_mapr'
-maintainer       'stephen.corbesero@synchronoss.com'
+maintainer       'yuvaraj.gopal@synchronoss.com'
 maintainer_email ''
 license          'All rights reserved'
 description      'Installs/Configures mapr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w( java sudo users yum ).each do |cb|
   depends cb
